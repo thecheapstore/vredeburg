fd8d4a1022063f2c2dd1ed2cbaeabe1b99cb3357
thecheapstore <jackwinchester943@gmail.com>
2024-08-17 01:17:20 +0530

    Changes in meta info

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,8 +1,8 @@
 module.exports = {
-  title: 'Vredeburg',
-  description: 'Eleventy starter project to create a blog',
-  keywords: ['eleventy', 'template', 'simple', 'clean'],
-  url: 'https://vredeburg.netlify.app', // your site url without trailing slash
+  title: 'The Cheap Store',
+  description: 'Welcome to The Cheap Store, your ultimate destination for finding unbeatable deals and discounts on a wide range of products. From daily deals and special offers to the best price comparisons across e-commerce sites, we bring you the hottest bargains so you never have to pay more again. Explore our curated deals and save big on everything you need.',
+  keywords: ['Daily deals', 'Discount coupons', 'Best price comparisons', 'E-commerce discounts', 'Bargain shopping', 'Cheap products', 'Online deals', 'Savings on shopping'],
+  url: 'https://thecheapstore.netlify.app/', // your site url without trailing slash
   paginate: 6 // how many posts you want to show for each page
   // uncomment the next line if you want to add disqus to your site
   // disqusShortname: "your-shortname"
